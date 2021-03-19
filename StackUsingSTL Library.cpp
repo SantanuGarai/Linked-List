7d554e593399681c3378ceb9b8cd724257aaeec7
SantanuGarai <68950806+SantanuGarai@users.noreply.github.com>
2021-03-19 10:02:14 +0530

    postfixEvalution function added

--- StackUsingSTL Library.cpp
+++ StackUsingSTL Library.cpp
@@ -36,6 +36,38 @@ void reverseStack(stack<int> &st){ // Reverse a Stack
     reverseStack(st);
     insertAtBottom(st,top);
 }
+int postfixEvalution(string s){  //Prefix Expression Evaluation
+    stack<int> st;
+    for(int i=s.length()-1;i>=0;i--){
+        if(s[i]>='0' && s[i]<='9'){
+            st.push(s[i]-'0');
+        }
+        else{
+            int operand1 = st.top();
+            st.pop();
+            int operand2 = st.top();
+            st.pop();
+            switch(s[i]){
+                case '+':
+                  st.push(operand1 + operand2);
+                  break;
+                case '-':
+                  st.push(operand1 - operand2);
+                  break;
+                case '*':
+                  st.push(operand1 * operand2);
+                  break;
+                case '/':
+                  st.push(operand1 / operand2);
+                  break;
+                case '^':
+                  st.push(pow(operand1, operand2));
+                  break;
+            }
+        } 
+   }
+   return st.top();
+}
 int main() {
 	string s = "I am santanu garai";
 	reverseSentence(s);
