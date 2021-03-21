9bf0d3d3646fbe905de0dc1388c3e3e06a798c9f
SantanuGarai <68950806+SantanuGarai@users.noreply.github.com>
2021-03-21 08:50:59 +0530

    infixToPostfix function added

--- StackUsingSTL Library.cpp
+++ StackUsingSTL Library.cpp
@@ -68,6 +68,49 @@ int postfixEvalution(string s){  //Prefix Expression Evaluation
    }
    return st.top();
 }
+/*  Infix to postfix conversion start */
+int prec(char c){
+    if(c == '^'){
+        return 3;
+    }
+    else if(c=='/' || c=='*'){
+        return 2;
+    }
+    else if (c=='+' || c=='-'){
+        return 1;
+    }
+    else{
+        return -1;
+    }
+}
+string infixToPostfix(string s){
+    stack<char> st;
+    string res;
+    for(int i=0;i<s.length();i++){
+        if((s[i]>='a' && s[i]<='z')|| (s[i]>='A' && s[i]<='Z')){
+            res+=s[i];
+        }
+        else if(s[i]=='('){
+            st.push(s[i]);
+        }
+        else if(s[i]==')'){
+            while(!st.empty() && st.top()!='('){
+                res+=st.top();
+                st.pop();
+            }
+            if(!st.empty()){
+                st.pop();
+            }
+        }
+        else{
+            while(!st.empty() && prec(st.top())>prec(s[i])){
+                res+=st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+    }
+}
+/* infix to postfix ends */
 int main() {
 	string s = "I am santanu garai";
 	reverseSentence(s);
