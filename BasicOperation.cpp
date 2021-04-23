bab3c0158baedcd30cd30b2b5d21afdc891d2997
SantanuGarai <68950806+SantanuGarai@users.noreply.github.com>
2021-04-23 17:43:36 +0530

    temporary changes

--- BasicOperation.cpp
+++ BasicOperation.cpp
@@ -316,3 +316,64 @@ int main() {
     display(newhead);
     return 0;
 }
+
+
+
+
+
+
+Given the head of a linked list, rotate the list to the right by k places.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+Example 2:
+
+
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 500].
+-100 <= Node.val <= 100
+0 <= k <= 2 * 109
+  
+  answer->
+  class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* temp = head;
+        int n = 0;
+        if(!head || !head->next){
+            return head;
+        }
+        while(temp){
+            n++;
+            temp = temp->next;
+        }
+        k = k%n;
+        while(fast && k){
+            fast = fast->next;
+            k--;
+        }
+        if(!fast){
+            return head;
+        }
+        while(fast->next){
+            fast = fast->next;
+            slow = slow->next;
+        }
+        fast->next=head;
+        head = slow->next;
+        slow->next = NULL;
+        return head;
+    }
+};
